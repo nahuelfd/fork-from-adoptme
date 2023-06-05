9e170a65ab5c338614386e98b50eb9a0b489279f
nahuelfd <nahuelfd@gmail.com>
2023-06-05 00:25:39 -0300

    arreglo nuevo

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import sessionsRouter from './routes/sessions.router.js';
 
 const app = express();
 const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(`MONGO_URL`)
+const connection = mongoose.connect(`process.env.MONGO_URL`)
 
 
 
